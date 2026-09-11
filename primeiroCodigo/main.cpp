256501e61d01a65285e54ef7b817be871e55dde1
Kayllon Victor <kevems004gmail.com>
2026-09-10 22:22:18 -0300

    fix: recupera funcoes perdidas do evento-01

--- primeiroCodigo/main.cpp
+++ primeiroCodigo/main.cpp
@@ -38,6 +38,15 @@ int ponto_2 = 0;
 
 
 //classes
+class Meteoro{
+    public:
+    float x, y;
+    int atualx = 0;
+    int atualy = 0;
+    int frame_fps;
+    bool ativo = false;
+
+};
 class Reto{
 public:
     float x, y, xx, yy;
@@ -53,14 +62,13 @@ public:
 };
 
 
-// funcoes de tratamento de evento
+//funçoes de tratamento de evento
 void Mouse(Reto botao, int estadoReal){
     Vector2 mouse = GetMousePosition();
     if(CheckCollisionPointRec(mouse, Rectangle{botao.x, botao.y, botao.xx, botao.yy}) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
         estado = estadoReal;
     }
 }
-
 void mov(Reto &reto){
     if(IsKeyDown(KEY_W)){
         reto.y -= reto.velocidade_y;
@@ -79,95 +87,92 @@ void Mov2(Reto &reto){
     }
 }
 
-// funcoes de atualizacao de posicoes
+//funçoes de atualizacao de posicoes
 void pingPong(Reto &reto, Reto &reto2, Reto &reto3, Reto &reto4, Bola &bola){
-    if(CheckCollisionCircleRec(Vector2{bola.x, bola.y}, bola.raio, Rectangle{reto.x, reto.y, reto.xx, reto.yy})){
-        if(bola.velocidade_x < 0){
-            PlaySound(somBatida);
-            bola.velocidade_y = 0.075 * (bola.y -(reto.y + (reto.yy /2)));
-            bola.velocidade_x *= -1.08;
+     if(CheckCollisionCircleRec(Vector2{bola.x, bola.y}, bola.raio, Rectangle{reto.x, reto.y, reto.xx, reto.yy})){
+            if(bola.velocidade_x < 0){
+               PlaySound(somBatida);
+               bola.velocidade_y = 0.075 * (bola.y -(reto.y + (reto.yy /2)));
+             bola.velocidade_x *= -1.08;
+            }
+
         }
-    }
+        if(bola.velocidade_x > 25){
+                bola.velocidade_x = 25;
+            }
+            if(bola.velocidade_x < -25){
+                bola.velocidade_x = -25;
+            }
+        if(CheckCollisionCircleRec(Vector2{bola.x, bola.y}, bola.raio, Rectangle{reto2.x, reto2.y, reto2.xx, reto2.yy})){
+            if(bola.velocidade_x > 0){
+               PlaySound(somBatida);
 
-    if(bola.velocidade_x > 25){
-        bola.velocidade_x = 25;
-    }
-    if(bola.velocidade_x < -25){
-        bola.velocidade_x = -25;
-    }
-
-    if(CheckCollisionCircleRec(Vector2{bola.x, bola.y}, bola.raio, Rectangle{reto2.x, reto2.y, reto2.xx, reto2.yy})){
-        if(bola.velocidade_x > 0){
-            PlaySound(somBatida);
-            bola.velocidade_y = 0.075 * (bola.y -(reto2.y + (reto2.yy /2)));
-            bola.velocidade_x *= -1.08;
-        }
-    }
-
-    if(bola.velocidade_x > 25){
-        bola.velocidade_x = 25;
-    }
-    if(bola.velocidade_x < -25){
-        bola.velocidade_x = -25;
-    }
-
-    if(CheckCollisionCircleRec(Vector2{bola.x, bola.y}, bola.raio, Rectangle{reto3.x, reto3.y, reto3.xx, reto3.yy})){
-        ponto_2 += 1;
-        PlaySound(somPonto);
-        bola.x = largura /2;
-        bola.y = altura /2;
-        bola.velocidade_x = 3;
-        bola.velocidade_y = 3;
-    }
-
-    if(CheckCollisionCircleRec(Vector2{bola.x, bola.y}, bola.raio, Rectangle{reto4.x, reto4.y, reto4.xx, reto4.yy})){
-        ponto_1 += 1;
-        PlaySound(somPonto);
-        bola.velocidade_x = 3;
-        bola.velocidade_y = 3;
-        bola.x = largura /2;
-        bola.y = altura /2;
-    }
-
-    bola.x += bola.velocidade_x;
-    bola.y += bola.velocidade_y;
-
-    if(bola.y >= altura - bola.raio){
-        PlaySound(somBatida2);
-        bola.velocidade_y *= -1;
-    }
-
-    if(bola.y <= + bola.raio){
-        PlaySound(somBatida2);
-        bola.velocidade_y *= -1;
-    }
-
-    if(reto.y <= 0 ){
-        reto.y += reto.velocidade_y;
-    }
-
-    if(reto.y >= altura - reto.yy){
-        reto.y -= reto.velocidade_y;
-    }
-
-    if(reto2.y <= 0 ){
-        reto2.y += reto2.velocidade_y;
-    }
-
-    if(reto2.y >= altura - reto2.yy){
-        reto2.y -= reto2.velocidade_y;
-    }
+               bola.velocidade_y = 0.075 * (bola.y -(reto2.y + (reto2.yy /2)));
+               bola.velocidade_x *= -1.08;
 }
+        }
+       if(bola.velocidade_x > 25){
+                bola.velocidade_x = 25;
+            }
+            if(bola.velocidade_x < -25){
+                bola.velocidade_x = -25;
+            }
+        if(CheckCollisionCircleRec(Vector2{bola.x, bola.y}, bola.raio, Rectangle{reto3.x, reto3.y, reto3.xx, reto3.yy})){
+            ponto_2 += 1;
+            PlaySound(somPonto);
+            bola.x = largura /2;
+            bola.y = altura /2;
+            bola.velocidade_x = 3;
+            bola.velocidade_y = 3;
+        }
+        if(CheckCollisionCircleRec(Vector2{bola.x, bola.y}, bola.raio, Rectangle{reto4.x, reto4.y, reto4.xx, reto4.yy})){
+            ponto_1 += 1;
+            PlaySound(somPonto);
+            bola.velocidade_x = 3;
+            bola.velocidade_y = 3;
+            bola.x = largura /2;
+            bola.y = altura /2;
+        }
 
+        bola.x += bola.velocidade_x;
+        bola.y += bola.velocidade_y;
+
+
+
+        if(bola.y >= altura - bola.raio){
+            PlaySound(somBatida2);
+
+            bola.velocidade_y *= -1;
+
+        }
+        if(bola.y <= + bola.raio){
+            PlaySound(somBatida2);
+
+            bola.velocidade_y *= -1;
+        }
+        if(reto.y <= 0 ){
+            reto.y += reto.velocidade_y;
+        }
+        if(reto.y >= altura - reto.yy){
+            reto.y -= reto.velocidade_y;
+        }
+        if(reto2.y <= 0 ){
+            reto2.y += reto2.velocidade_y;
+        }
+        if(reto2.y >= altura - reto2.yy){
+            reto2.y -= reto2.velocidade_y;
+        }
+        }
 void Ia(Bola &bola, Reto &reto2){
-    if(bola.y > reto2.y){
-        reto2.y += reto2.velocidade_y;
-    }
-    if(bola.y < reto2.y){
-        reto2.y -= reto2.velocidade_y;
-    }
+        if(bola.y > reto2.y){
+               reto2.y += reto2.velocidade_y;}
+              if(bola.y < reto2.y){
+              reto2.y -= reto2.velocidade_y;}
 }
-//funï¿½oes de desenhar
+
+//funcoes de desenhar
+Meteoro chuva[5];
+Reto boom;
 Reto botao2;
 Reto botao;
 Reto reto4;
@@ -184,12 +189,65 @@ Bola bola;
 void Drawl(Bola bola){
     DrawCircle(bola.x, bola.y, bola.raio, bola.cor);
 };
+
+void meteoros(Texture2D meteoro,Meteoro chuva[5], Reto &boom) {
+    if(evento_acontecendo == true){
+    for(int i = 0; i < 5; i++){
+        if(chuva[i].ativo == false){
+            chuva[i].atualx = 0;
+            chuva[i].atualy = 0;
+            chuva[i].frame_fps = 0;
+            uniform_int_distribution<int> ale_largura(300, largura - 300);
+            uniform_int_distribution<int> ale_altura(300, altura - 300);
+            chuva[i].x = ale_largura(gen);
+            chuva[i].y = ale_altura(gen);
+            chuva[i].ativo = true;
+        }
+    }
+    evento_acontecendo = false;
+    }
+
+    for(int i = 0; i <5; i++){
+      if(chuva[i].ativo == true){
+
+       chuva[i].frame_fps++;
+    if(chuva[i].frame_fps == 10){
+       chuva[i].frame_fps = 0;
+       chuva[i].atualx++;
+
+
+    if(chuva[i].atualx > 4 ){
+        chuva[i].atualx = 0;
+        chuva[i].atualy++;
+
+    }
+
+     if(chuva[i].atualy > 2){
+        chuva[i].atualx = 0;
+        chuva[i].atualy = 0;
+        chuva[i].ativo = false;
+
+    }
+
+
+    }
+    if(chuva[i].ativo == true) {
+    float imagem_origemx = chuva[i].atualx * boom.xx * 2.5;
+    float imagem_origemy = chuva[i].atualy * boom.yy * 2.5;
+    boom.x = chuva[i].x;
+    boom.y = 0;
+    DrawlReto(boom);
+    DrawTexturePro(meteoro, (Rectangle){imagem_origemx, imagem_origemy, boom.xx * 2.5 , boom.yy * 2.5}, (Rectangle){chuva[i].x, 0, boom.xx , boom.yy }, {0,0}, 0, WHITE);
+}
+}
+}
+}
 //////////////////main.
 int main() {
     /////////////main.
     ///////////tela.
 
-
+    SetConfigFlags(FLAG_WINDOW_HIDDEN | FLAG_WINDOW_RESIZABLE);
     InitWindow(largura, altura, "qualquer");
 
     int monitor = GetCurrentMonitor();
@@ -198,26 +256,33 @@ int main() {
     SetWindowSize(largura, altura);
     SetWindowPosition(0, 0);
     ClearWindowState(FLAG_WINDOW_HIDDEN);   //////////tela.
-    SetConfigFlags(FLAG_WINDOW_HIDDEN | FLAG_WINDOW_RESIZABLE);
+
+
+    //texturas
+    Texture2D meteoro = LoadTexture("meteoro.png.png");
+
      //sons
     InitAudioDevice();
     somPonto = LoadSound("ponto.wav.wav");
     somBatida = LoadSound("raquete.wav.wav");
     somBatida2 = LoadSound("soun.wav.wav");
 
+
+
+
     SetTargetFPS(150);
 
     botao.cor = azul;
-    botao.xx = 500;
-    botao.x = largura /2 - 250;
-    botao.yy = 100;
-    botao.y = altura /2 - 50 - 100;
+    botao.xx = 400;
+    botao.x = largura /2 - 200;
+    botao.yy = 70;
+    botao.y = altura /2 - 35 - 100;
 
     botao2.cor = vermelho;
-    botao2.xx = 500;
-    botao2.x = largura /2 - 250;
-    botao2.yy = 100;
-    botao2.y = altura /2 + 50 + 60;
+    botao2.xx = 200;
+    botao2.x = largura /2 - 100;
+    botao2.yy = 70;
+    botao2.y = altura /2 + 35 + 60;
 
     reto.cor = WHITE;
     reto.velocidade_x = 7;
@@ -262,7 +327,7 @@ int main() {
     boom.y = 0;
     boom.yy = (float)meteoro.height /3 * 0.4;
 
-    // loop
+    //loop
     while(!WindowShouldClose()){
         largura = GetScreenWidth();
         altura = GetScreenHeight();
@@ -271,41 +336,71 @@ int main() {
 
 
         //tratamento de eventos
+        frame++;
+
+        if(frame == evento_tempo ){
+            aleatorio = evento(gen);
+            numero_de_eventos = aleatorio;
+            if(numero_de_eventos != 0){
+            evento_acontecendo = true;
+            frame_fps = 0;
+            }
+            else{
+            evento_acontecendo = false;
+
+            }
+            frame = 0;
+        }
         if(estado == menu){
             Mouse(botao, ia);
             Mouse(botao2, j2);
         }
         if(estado == ia){
-            mov(reto);
+
+        mov(reto);
+
         }
         else if(estado == j2){
-            mov(reto);
-            Mov2(reto2);
+             mov(reto);
+        Mov2(reto2);
         }
-
-        // atualizacao de posicoes
+        //atualizacao de posicoes
         if(estado == ia){
             pingPong(reto, reto2, reto3, reto4, bola);
             if(bola.x > largura / 2){
-                Ia(bola, reto2);
-            }
+            Ia(bola, reto2);
+        }
         }
         if(estado == j2){
             pingPong(reto, reto2, reto3, reto4, bola);
         }
         //desenhar
-
         BeginDrawing();
         if(estado == menu){
             ClearBackground(BLACK);
             DrawlReto(botao);
             DrawlReto(botao2);
-            DrawText(TextFormat("pong de mesa"),largura /3, 0, 100, WHITE);
-            DrawText(TextFormat("modo ia"),largura /2 - 100, botao.y + 20, 50, WHITE);
-            DrawText(TextFormat("modo 2 jogadores"),largura /2 - 220, botao2.y + 20, 50, WHITE);
+             DrawText(TextFormat("1j"),largura /3, 0, 80, WHITE);
         }
         else if(estado == ia){
             ClearBackground(verde);
+        DrawlReto(reto);
+        DrawlReto(reto2);
+        DrawLine(largura / 2, 0, largura/2, altura, WHITE);
+        DrawCircleLines(largura /2, altura/2, 300, WHITE);
+        DrawlReto(reto3);
+        DrawlReto(reto4);
+        if(numero_de_eventos == nu_meteoro){
+                meteoros(meteoro,chuva, boom);
+            }
+
+        DrawText(TextFormat("%d",ponto_1),largura /3, 0, 80, WHITE);
+        DrawText(TextFormat("%d",ponto_2),largura /1.5, 0, 80, WHITE);
+        Drawl(bola);}
+
+
+        else if(estado == j2){
+        ClearBackground(verde);
         DrawlReto(reto);
         DrawlReto(reto2);
         DrawLine(largura / 2, 0, largura/2, altura, WHITE);
@@ -316,25 +411,11 @@ int main() {
         DrawText(TextFormat("%d",ponto_2),largura /1.5, 0, 80, WHITE);
         Drawl(bola);}
 
-
-        else if(estado == j2){
-            ClearBackground(verde);
-            DrawlReto(reto);
-            DrawlReto(reto2);
-            DrawLine(largura / 2, 0, largura/2, altura, WHITE);
-            DrawCircleLines(largura /2, altura/2, 300, WHITE);
-            DrawlReto(reto3);
-            DrawlReto(reto4);
-            DrawText(TextFormat("%d",ponto_1),largura /3, 0, 80, WHITE);
-            DrawText(TextFormat("%d",ponto_2),largura /1.5, 0, 80, WHITE);
-            Drawl(bola);
-        }
-
         EndDrawing();
     }
+    UnloadTexture(meteoro);
     UnloadSound(somBatida);
     CloseAudioDevice();
     CloseWindow();
     return 0;
 }
-
